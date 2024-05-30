ee5f19220601bac60c56fc0a53445913d4700812
elbeko19 <154595333+elbeko19@users.noreply.github.com>
2024-05-30 14:42:08 +0500

    Add files via upload

---
+++ imgUpload/project/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Image(models.Model):
+    caption = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='img/%y')
+    
+    def __str__(self):
+        return self.caption
